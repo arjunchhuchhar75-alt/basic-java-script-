2ff6f8c0612d73a30f19fac750eeca0e3dc9b03b
Arjun <arjunchhuchhar75@gmail.com>
2026-08-25 11:18:11 +0530

    js question

---
+++ Dsa.js
@@ -0,0 +1,140 @@
+// let arr = [1, 2, 3, 4, 5]
+
+// let result = arr.map((nums) => nums*2)
+     
+      
+
+
+// // console.log(result);
+
+// let arr = [5, 10, 15, 20]
+
+// let result = arr.map((nums)=> nums + 10 )
+
+// console.log(result);
+
+// let names = ["arjun", "rahul", "amit"]
+
+// let result = names.map(name => name.toUpperCase()) 
+
+// // console.log(result);
+
+// let arr = [2,3,4,5]
+
+// let result = arr.map((nums) => nums*nums)
+
+// console.log(result);
+
+// let price = [100,200,300,400]
+
+// let result = price.map((nums) => nums + 50)
+
+// // console.log(result);
+
+// let users = [
+//     {name: "Arjun", age: 18},
+//     {name: "Rahul", age: 20},
+//     {name: "Amit", age: 17}
+// ];
+
+// let result = users.map(user => user.name)
+
+// // console.log(result);
+
+// let arr = [5, 12, 8, 20, 15, 3]
+
+// let result = arr.filter("evennumbers is greater than 10")
+
+// console.log(result);
+
+
+// let arr = [1,2,3,4,5,6]
+
+// let Evennumbers = arr.filter((Number) = Number % 2)
+
+// // console.log(Evennumbers);
+// let arr = [20, 40, 90]
+// let smallestnumbers = arr[0]
+//   for (let i = 0; i < arr.length; i++) {
+//      if(arr[i] < smallestnumbers)
+//               smallestnumbers = arr[i]
+// }
+// console.log(smallestnumbers);
+
+
+
+// let arr = [7,4,3,5,11,34,60]
+// let countEvennums = arr[countEvennums]
+
+// for (let i = 0; i < arr.length; i++) {
+//     if(arr[i] % 2 === 0)
+//         countEvennums = 
+//         + countEvennums
+    
+// }
+// console.log(countEvennums);
+
+
+let arr = [10,20,30,40]
+
+function something() {
+for (let index = 0; index < arr.length; index++) {
+    if (arr[index]=== arr[2]) {
+    return true
+        
+       }         
+    }
+    return false
+}
+
+
+
+
+console.log(something());
+
+// let arr = [1,2,3,4,5]
+
+// function reverse(){
+    
+//     for (let index = 4; index < array.length; index--) {
+//       return reverse 
+    
+    
+// }
+// }
+// console.log(reverse);
+
+// let arr = [10,20,30,40]
+
+
+  
+//  function something() {
+  
+//  }       for (let index = 0; index < arr.length; index++) {
+//       if (arr[index] === arr[2]) {
+//            return true
+//       }
+
+// }
+
+// return false
+// console.log(something);
+
+// let a = 10
+// let b = 20
+// let c = 30
+
+// if (a>b & a>c) {
+//    console.log(a) 
+// }else if (b>a & b>c){
+//     console.log(b)
+// }else if (c>a & c>b) {
+//      console.log(c)  
+// } 
+    
+
+    
+
+
+    
+
