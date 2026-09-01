a0210acbaacacf391ead1a439ad402509666243c
Arjun <arjunchhuchhar75@gmail.com>
2026-09-01 11:18:19 +0530

    javascript-question/find-the-second-largest-number.js

---
+++ javascript-question/find-the-second-largest-number.js
@@ -0,0 +1,23 @@
+
+ let arr = [10, 20, 30, 40, 50, 60];
+
+let largest = arr[0];
+let secondlargest = arr[0];
+for (let index = 0; index < arr.length; index++) {
+    
+   if (arr[index] > largest) {
+       largest = arr[index]
+     
+    }
+   }
+for (let index = 0; index < arr.length; index++) {
+     if (arr[index] === largest) {
+      continue
+    }
+      if (arr[index]  >  secondlargest) {
+    secondlargest = arr[index]
+}
+
+} 
+
+   console.log(secondlargest)
