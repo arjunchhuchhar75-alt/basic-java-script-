28051ec5f5f1be3fe4705212e77eabb3c70ebae3
Arjun <arjunchhuchhar75@gmail.com>
2026-09-01 13:21:42 +0530

    Handle edge case ib largest fun

--- javascript-question/find-first-largest.js
+++ javascript-question/find-first-largest.js
@@ -1,7 +1,10 @@
 let arr = [3, 5, 7, 2, 8, 1, 4];
-let largest = arr[0];
 
 function something(arr) {
+    if(arr.length === 0) {
+        return null;
+    }
+let largest = arr[0];
     for (let i = 1; i < arr.length; i++) {
         if (arr[i] > largest) {
             largest = arr[i];
