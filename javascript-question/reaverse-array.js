535fc46f85af3c17483e3c6946ab9cd6beb818e3
Arjun <arjunchhuchhar75@gmail.com>
2026-09-04 11:53:20 +0530

    find-reverse-array-in-help-to-function

--- javascript-question/reaverse-array.js
+++ javascript-question/reaverse-array.js
@@ -1,7 +1,9 @@
-let arr = [10, 20, 30, 40, 50, 60];
-let reversed = []
-for (let index = arr.length - 1; index >= 0; index--) {
-    reversed.push(arr[index])
+function something(arr) {
+    let reversed = []
+    for (let index = arr.length - 1; index >= 0; index--) {
+        reversed.push(arr[index])
+    }
+    return reversed
 }
 
-console.log(reversed)
+console.log(something([10, 20, 30, 40, 50, 60]))
